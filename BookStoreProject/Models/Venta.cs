964869dd5e6a11ce5a239350dca3f86c7709e9cc
Jatzso <91919512+Jatzso@users.noreply.github.com>
2021-12-15 13:51:16 -0300

    Formulario compra

--- BookStoreProject/Models/Venta.cs
+++ BookStoreProject/Models/Venta.cs
@@ -13,47 +13,47 @@ namespace BookStoreProject.Models
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         
-        [Required(ErrorMessage = "El campo es requerido")]
-        [RegularExpression(@"[A-Za-z]{3,}", ErrorMessage = "El nombre debe contener al menos 3 caracteres")]
+        [Required(ErrorMessage = "*Campo requerido")]
+        [RegularExpression(@"[A-Za-z]{3,}", ErrorMessage = "*Campo inválido")]
         public string Nombre { get; set; }
         
-        [Required(ErrorMessage = "El campo es requerido")]
-        [RegularExpression(@"[A-Za-z]{3,}", ErrorMessage = "El apellido debe contener al menos 3 caracteres")]
+        [Required(ErrorMessage = "*Campo requerido")]
+        [RegularExpression(@"[A-Za-z]{3,}", ErrorMessage = "*Campo inválido")]
         public string Apellido { get; set; }
         
-        [Required(ErrorMessage = "El campo es requerido")]
+        [Required(ErrorMessage = "*Campo requerido")]
         [Range(1000000, 990000000)]
-        [RegularExpression(@"[0-9]{8}", ErrorMessage = "El DNI ingresado es inválido")]
+        [RegularExpression(@"[0-9]{8}", ErrorMessage = "*DNI inválido")]
         public int Dni { get; set; }
         
-        [Required(ErrorMessage = "El campo es requerido")]
-        [RegularExpression(@"[A-Za-z ]{3,}", ErrorMessage = "La calle debe contener al menos 3 caracteres")]
+        [Required(ErrorMessage = "*Campo requerido")]
+        [RegularExpression(@"[A-Za-z ]{3,}", ErrorMessage = "*Calle inválida")]
         public string Calle { get; set; }
 
-        [Required(ErrorMessage = "El campo es requerido")]
-        [RegularExpression(@"[0-9]{1,4}", ErrorMessage = "La altura debe contener mínimo un número")]
+        [Required(ErrorMessage = "*Campo requerido")]
+        [RegularExpression(@"[0-9]{1,4}", ErrorMessage = "*Altura inválida")]
         public int Altura { get; set; }
 
-        [Required(ErrorMessage = "El campo es requerido")]
-        [RegularExpression(@"[A-Za-z ]{4,}", ErrorMessage = "La provincia debe contener al menos 4 caracteres")]
+        [Required(ErrorMessage = "*Campo requerido")]
+        [RegularExpression(@"[A-Za-z ]{4,}", ErrorMessage = "*Provincia inválida")]
         public string Provincia { get; set; }
         
-        [Required(ErrorMessage = "El campo es requerido")]
+        [Required(ErrorMessage = "*Campo requerido")]
         [Phone]
-        [RegularExpression(@"11[0-9]{8}", ErrorMessage = "El teléfono es inválido")]
+        [RegularExpression(@"11[0-9]{8}", ErrorMessage = "*Teléfono inválido")]
         public int Telefono { get; set; }
 
-        [Required(ErrorMessage = "El campo es requerido")]
-        [EmailAddress]
-        [RegularExpression(@"^[^@]+@[^@]+\.[a-zA-Z]{2,}$", ErrorMessage = "El correo es inválido")]
+        [Required(ErrorMessage = "*Campo requerido")]
+        [EmailAddress(ErrorMessage = "*Email inválido")]
+        [RegularExpression(@"^[^@]+@[^@]+\.[a-zA-Z]{2,}$", ErrorMessage = "*Formato inválido")]
         public string Email { get; set; }
 
-        [Required(ErrorMessage = "El campo es requerido")]
+        [Required(ErrorMessage = "*Campo requerido")]
         public string Tarjeta { get; set; }
 
-        [Required(ErrorMessage = "El campo es requerido")]
+        [Required(ErrorMessage = "*Campo requerido")]
         [CreditCard]
-        [RegularExpression(@"5[1-5][0-9]{14}$", ErrorMessage = "La tarjeta es inválida")]
+        [RegularExpression(@"5[1-5][0-9]{14}$", ErrorMessage = "*Nro° tarjeta inválida")]
         public int NumeroTarjeta { get; set; }
 
         //Relaciones con otras entidades
